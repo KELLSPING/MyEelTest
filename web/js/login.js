aad88ca5054cd916fd328da58f6f0b52abc3de78
Kells_P <kellsping@gmail.com>
2023-12-07 19:35:26 +0800

    Update

--- web/js/login.js
+++ web/js/login.js
@@ -2,9 +2,26 @@
 //所以需要用async function 加上await去呼叫PY function
 
 async function checkAndRedirect() {
-    var inputName = document.getElementById("btnCommit").value;
-    const result = await eel.strAdd(inputName)()  
+    var result = "" 
     
     //最後將返回的值設定在HTML上的<p>內
     document.getElementById('pError').innerText  = result
+
+
+    var inputName = document.getElementById("nameInput").value;
+    var selectLang = document.getElementById("dropdown").value;
+
+    if (inputName.trim() === "" && selectLang.trim() === "") {
+        result = "\"Language\" and \"Name\" are empty."
+        document.getElementById('pError').innerText  = result
+    } else if (inputName.trim() === "") {
+        result = "\"Name\" is empty."
+        document.getElementById('pError').innerText  = result
+    } else if (selectLang.trim() === "") {
+        result = "\"Language\" is empty."
+        document.getElementById('pError').innerText  = result
+    } else {
+        result = "Great"
+        document.getElementById('pError').innerText  = result
+    }
 }
