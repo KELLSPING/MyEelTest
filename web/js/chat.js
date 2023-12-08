985d385be5e82c158848814c2fa7e5ec50077a9c
Kells_P <kellsping@gmail.com>
2023-12-08 14:57:08 +0800

    Update.

--- web/js/chat.js
+++ web/js/chat.js
@@ -1,7 +1,7 @@
 // 获取 URL 中的查询参数
 const urlParams = new URLSearchParams(window.location.search);
-const userInputName = urlParams.get('userInputName');
-const userLangSelect = urlParams.get('userLangSelect');
+const userInputName = urlParams.get("userInputName");
+const userLangSelect = urlParams.get("userLangSelect");
 
 // 顯示名字和語言在 chat.html
 // document.getElementById('txtName').innerText  = userInputName;
@@ -11,50 +11,50 @@ eel.process_user_input(userInputName, userLangSelect);
 
 // right key forbidden
 document.oncontextmenu = new Function("return false");
-oncontextmenu="return flase;"
+oncontextmenu = "return flase;";
 
 // 鍵盤監聽
-document.onkeydown = function(e){
-  var keyNum =window.event ? e.keyCode : e.which;
+document.onkeydown = function (e) {
+  var keyNum = window.event ? e.keyCode : e.which;
   //Enter press
-  if (keyNum == 13){
+  if (keyNum == 13) {
     send_message();
   }
-}
+};
 
 // 這邊必須要async funciton 因為python返回需要時間，而JS 又不會block，
 // 所以需要用async function 加上await去呼叫PY function
-async function send_message(){
-    var messageInput = document.getElementById('message-input');
-    var message = messageInput.value.trim();
-  
-  if (message !== '') {
-    // 呼叫 eel 中的 func ，func 名稱後面的 () 作為輸入參數用，最後的 () 用作取值，並將結果回傳在 result
-    result = await eel.get_message(message)()
+async function send_message() {
+  var messageInput = document.getElementById("message-input");
+  var message = messageInput.value.trim();
 
-    var chatMessages = document.getElementById('chat-messages');
-    var newMessage = document.createElement('div');
+  if (message !== "") {
+    // 呼叫 eel 中的 func ，func 名稱後面的 () 作為輸入參數用，最後的 () 用作取值，並將結果回傳在 result
+    result = await eel.get_message(message)();
+
+    var chatMessages = document.getElementById("chat-messages");
+    var newMessage = document.createElement("div");
     newMessage.textContent = result;
-    
+
     // 使用 JS 實現文字隨視窗大小縮放
     setResponsiveFontSize(newMessage, "16px");
 
     chatMessages.appendChild(newMessage);
     // Clear the input field
-    messageInput.value = '';
+    messageInput.value = "";
     // Scroll to the bottom to show the latest message
     chatMessages.scrollTop = chatMessages.scrollHeight;
   }
 }
-  
+
 eel.expose(update);
-function update(s){
+function update(s) {
   var message = s;
-  if (message !== '') {
+  if (message !== "") {
     // 呼叫 eel 中的 func ，func 名稱後面的 () 作為輸入參數用，最後的 () 用作取值，並將結果回傳在 result
 
-    var chatMessages = document.getElementById('chat-messages');
-    var newMessage = document.createElement('div');
+    var chatMessages = document.getElementById("chat-messages");
+    var newMessage = document.createElement("div");
     newMessage.textContent = s;
     chatMessages.appendChild(newMessage);
     // Clear the input field
@@ -62,27 +62,27 @@ function update(s){
     chatMessages.scrollTop = chatMessages.scrollHeight;
   }
 }
-  
-function mouseDown(){
-    var button = document.getElementById('btn-send-audio');
-    button.classList.add('clicked');
-    eel.on_press()()
+
+function mouseDown() {
+  var button = document.getElementById("btn-send-audio");
+  button.classList.add("clicked");
+  eel.on_press()();
 }
-  
-function mouseUp(){
-  var button = document.getElementById('btn-send-audio');
-  var chatMessages = document.getElementById('chat-messages');
-  var newMessage = document.createElement('div');
-  button.classList.remove('clicked');
-  eel.on_release()()
+
+function mouseUp() {
+  var button = document.getElementById("btn-send-audio");
+  var chatMessages = document.getElementById("chat-messages");
+  var newMessage = document.createElement("div");
+  button.classList.remove("clicked");
+  eel.on_release()();
 }
-  
+
 eel.expose(showText);
-function showText(s){
-    var messageInput = document.getElementById('message-input');
-    messageInput.value=s;
+function showText(s) {
+  var messageInput = document.getElementById("message-input");
+  messageInput.value = s;
 }
-  
+
 function setResponsiveFontSize(element, initialFontSize) {
   // Set the initial font size (optional)
   element.style.fontSize = initialFontSize;
@@ -90,7 +90,8 @@ function setResponsiveFontSize(element, initialFontSize) {
   // Add a listener for window resize events to update font size responsively
   function updateFontSize() {
     // Get the current viewport width
-    var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    var viewportWidth =
+      window.innerWidth || document.documentElement.clientWidth;
 
     // Adjust the font size based on the viewport width
     if (viewportWidth < 600) {
@@ -101,20 +102,19 @@ function setResponsiveFontSize(element, initialFontSize) {
       element.style.fontSize = "32px";
     }
   }
-  
+
   // Add the new message div to the chat-messages container
-  var chatMessages = document.getElementById('chat-messages');
+  var chatMessages = document.getElementById("chat-messages");
   chatMessages.appendChild(element);
 
   // Trigger the resize event once to set the initial font size
   updateFontSize();
 
   // Add event listener for window resize
-  window.addEventListener('resize', updateFontSize);
+  window.addEventListener("resize", updateFontSize);
 } // setResponsiveFontSize
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
   // This function will be called when the page is about to be closed
-  eel.close_chat();  // Call the Python function
+  eel.close_chat(); // Call the Python function
 };
-  
--- web/js/login.js
+++ web/js/login.js
@@ -1,52 +1,55 @@
 function checkAndRedirect() {
-    var result = "" 
+  var result = "";
 
-    var inputName = document.getElementById("nameInput").value;
-    var selectLang = document.getElementById("dropdown").value;
+  var inputName = document.getElementById("nameInput").value;
+  var selectLang = document.getElementById("dropdown").value;
 
-    // check both language and name are empty
-    if (inputName.trim() === "" && selectLang.trim() === "") {
-        result = "\"Language\" and \"Name\" are empty."
-        document.getElementById('pError').innerText  = result
-    } 
-    // check language is empty
-    else if (selectLang.trim() === "") {
-        result = "\"Language\" is empty."
-        document.getElementById('pError').innerText  = result
-    } 
-    // check name is empty
-    else if (inputName.trim() === "") {
-        result = "\"Name\" is empty."
-        document.getElementById('pError').innerText  = result
-    } 
-    // check name is over 10 characters
-    else if (inputName.length > 10) {
-        result = "\"Name\" is too long."
-        document.getElementById('pError').innerText  = result
-    } 
-    else {
-        // check name exist or not
-        checkNameExist(inputName, selectLang);
-    }
+  // check both language and name are empty
+  if (inputName.trim() === "" && selectLang.trim() === "") {
+    result = '"Language" and "Name" are empty.';
+    document.getElementById("pError").innerText = result;
+  }
+  // check language is empty
+  else if (selectLang.trim() === "") {
+    result = '"Language" is empty.';
+    document.getElementById("pError").innerText = result;
+  }
+  // check name is empty
+  else if (inputName.trim() === "") {
+    result = '"Name" is empty.';
+    document.getElementById("pError").innerText = result;
+  }
+  // check name is over 10 characters
+  else if (inputName.length > 10) {
+    result = '"Name" is too long.';
+    document.getElementById("pError").innerText = result;
+  } else {
+    // check name exist or not
+    checkNameExist(inputName, selectLang);
+  }
 }
 
-window.onbeforeunload = function() {
-    // This function will be called when the page is about to be closed
-    eel.close_login();  // Call the Python function
+window.onbeforeunload = function () {
+  // This function will be called when the page is about to be closed
+  eel.close_login(); // Call the Python function
 };
 
 async function checkNameExist(inputName, selectLang) {
-    var result = ""
+  var result = "";
 
-    // 等待 1 秒，作為檢查資料庫中使否依樣名字的等待時間
-    await new Promise(resolve => setTimeout(resolve, 1000));
+  // 等待 1 秒，作為檢查資料庫中使否依樣名字的等待時間
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    var ans = await eel.nameAlreadyExist(inputName)()
-    console.log(result);
-    if (ans === true) {
-        result = "\"Name\" is already exist."
-        document.getElementById('pError').innerText  = result
-    } else {
-        window.location.href = "chat.html?userLangSelect=" + encodeURIComponent(selectLang) + "&userInputName=" + encodeURIComponent(inputName);
-    }
+  var ans = await eel.nameAlreadyExist(inputName)();
+  console.log(result);
+  if (ans === true) {
+    result = '"Name" is already exist.';
+    document.getElementById("pError").innerText = result;
+  } else {
+    window.location.href =
+      "chat.html?userLangSelect=" +
+      encodeURIComponent(selectLang) +
+      "&userInputName=" +
+      encodeURIComponent(inputName);
+  }
 }
